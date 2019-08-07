14b7111b505adb636794c706b1093f4c85c36229
desertsniper87 <torshobuet@gmail.com>
2019-08-08 04:19:49 +0600

    basic user model

---
+++ lab_test_REST/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class UserProfile(models.Model):
+  user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+  role = models.CharField(choices=['buyer', 'seller'])
--- lab_test_REST/serializers.py
+++ lab_test_REST/serializers.py
@@ -1,10 +1,21 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('url', 'password', 'username', 'email', 'groups')
+
+    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
+    username = serializers.CharField( validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(min_length=8)
+
+    def create(self, validated_data):
+        user = User.objects.create(username=validated_data['username'],
+                                   email=validated_data['email'],
+                                   password=validated_data['password'])
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
