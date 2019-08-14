7cd1cd9ccf02b64b80ede1f096fc3acbb2305348
desertsniper87 <torshobuet@gmail.com>
2019-08-14 16:39:00 +0600

    static folders

--- lab_test_REST/settings.py
+++ lab_test_REST/settings.py
@@ -173,8 +173,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-                        os.path.join(BASE_DIR, "static")
-                   ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
