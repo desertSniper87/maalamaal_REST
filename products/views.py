1dc9a5432405eea7c3f3ec5661cae4b9e8bfac1d
desertsniper87 <torshobuet@gmail.com>
2019-08-15 15:30:59 +0600

    specific product retrieval

--- products/views.py
+++ products/views.py
@@ -17,7 +17,6 @@ class ProductViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         return super(ProductViewSet, self).create(request, *args, **kwargs)
 
-
     def get_permissions(self):
         try:
             # return permission_classes depending on `action`
@@ -25,6 +24,19 @@ class ProductViewSet(viewsets.ModelViewSet):
         except KeyError:
             # action is not set return default permission_classes
             return [permission() for permission in self.permission_classes]
+
+    def list(self, request, *args, **kwargs):
+        category = request.GET.get('category')
+        if category:
+            self.queryset = Product.objects.filter(category=category)
+        user = request.user
+        user_type = user.groups.first()
+        if user_type and user_type.name == 'seller':
+            self.queryset = self.queryset.filter(seller=user)
+
+        return super(ProductViewSet, self).list(request, *args, **kwargs)
+
+
 
 
 class ProductCategoryViewSet(viewsets.ModelViewSet):
